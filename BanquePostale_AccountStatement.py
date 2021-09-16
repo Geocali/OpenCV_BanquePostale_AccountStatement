b48f664c19a1520e04d80b984bcee483e3a641e9
cvial <clement.vial@alstomgroup.com>
2021-09-16 18:02:10 +0200

    started refactoring

--- BanquePostale_AccountStatement.py
+++ BanquePostale_AccountStatement.py
@@ -44,54 +44,54 @@ import json
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
 
-#-> read banque account parameters from json file:
-try:
-    accountFileName = 'BanquePostale_account.json'
-    with open(accountFileName) as json_file:
-        data = json.load(json_file)
-except:
-    ThisError = sys.exc_info()[0]
-    print("Error trying to import file",accountFileName,":",ThisError.__name__)
-    sys.exit() #stop the script
+def get_account_params():
 
-try:
-    param_NumeroDeCompte = data['account'][0]['param_NumeroDeCompte']
-    param_ID = data['account'][0]['param_ID']
-    param_PWD = data['account'][0]['param_PWD']
-    param_DownloadFolder =  data['account'][0]['param_DownloadFolder']
-    param_HEADLESS_PROCESS =  data['account'][0]['param_HEADLESS_PROCESS']
-except :
-    print("Incorrect input file format:",sys.exc_info())
-    sys.exit() #stop the script
+    #-> read banque account parameters from json file:
+    params = {}
+    try:
+        accountFileName = 'BanquePostale_account.json'
+        with open(accountFileName) as json_file:
+            data = json.load(json_file)
+    except:
+        ThisError = sys.exc_info()[0]
+        print("Error trying to import file",accountFileName,":",ThisError.__name__)
+        sys.exit() #stop the script
 
-print("Parameters extracted from input file: ",param_NumeroDeCompte,    \
-    '/',param_ID,'/',param_PWD,'/',param_DownloadFolder,'/',            \
-    param_HEADLESS_PROCESS)
+    try:
+        params['NumeroDeCompte'] = data['account'][0]['param_NumeroDeCompte']
+        params['ID'] = data['account'][0]['param_ID']
+        params['PWD'] = data['account'][0]['param_PWD']
+        params['DownloadFolder'] =  data['account'][0]['param_DownloadFolder']
+        params['HEADLESS_PROCESS'] =  data['account'][0]['param_HEADLESS_PROCESS']
+    except :
+        print("Incorrect input file format:",sys.exc_info())
+        sys.exit() #stop the script
 
-try:
+    print("Parameters extracted from input file: ",params['NumeroDeCompte'],    \
+        '/',params['ID'],'/',params['PWD'],'/',params['DownloadFolder'],'/',            \
+        params['HEADLESS_PROCESS'])
+    return params
+
+def create_driver():
     options = Options()
 
-    if (param_HEADLESS_PROCESS == "True"):
+    if (params['HEADLESS_PROCESS'] == "True"):
         options.add_argument("--headless")
 
     options.set_preference("browser.download.folderList", 2)
-    options.set_preference("browser.download.dir", param_DownloadFolder)
+    options.set_preference("browser.download.dir", params['DownloadFolder'])
     options.set_preference("browser.download.useDownloadDir", True)
     options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
     options.set_preference("pdfjs.disabled", True)  # disable the built-in PDF viewer
     driver = webdriver.Firefox(executable_path=curr_path + '/geckodriver', options=options)
-    driver.get("https://voscomptesenligne.labanquepostale.fr/wsost/OstBrokerWeb/loginform?TAM_OP=login&ERROR_CODE=0x00000000&URL=%2Fvoscomptes%2FcanalXHTML%2Fidentif.ea%3Forigin%3Dparticuliers")
+    return driver
 
-except Exception as e:
-    print("Error when opening webpage:",str(e).replace('%20',' '))
-    sys.exit() #stop the script
+params = get_account_params()
+driver = create_driver()
+driver.get("https://voscomptesenligne.labanquepostale.fr/wsost/OstBrokerWeb/loginform?TAM_OP=login&ERROR_CODE=0x00000000&URL=%2Fvoscomptes%2FcanalXHTML%2Fidentif.ea%3Forigin%3Dparticuliers")
 
-# print(driver.page_source)  #to display whole page HTML
-
-elt_val_cel_identifiant = driver.find_element_by_id("val_cel_identifiant").send_keys(param_ID)
-
-dictPWD = {param_PWD[0]:"",param_PWD[1]:"",param_PWD[2]:"",param_PWD[3]:"",param_PWD[4]:"",param_PWD[5]:""}
-listPWD = list(dictPWD)
+# send id to form
+driver.find_element_by_id("val_cel_identifiant").send_keys(params['ID'])
 
 #-> get each image of each button from the virtual keyboard
 os.makedirs("img", exist_ok=True)
@@ -104,15 +104,17 @@ for lineNum in range(16):
     index += 1
 
 #-> Match for first pwd digit
-for element in range(len(listPWD)):
+uniq_digits_in_pwd = set(params['PWD'])
+dictPWD = {params['PWD'][0]:"",params['PWD'][1]:"",params['PWD'][2]:"",params['PWD'][3]:"",params['PWD'][4]:"",params['PWD'][5]:""}
+for element in range(len(uniq_digits_in_pwd)):
 
     # load Reference image for the current password digit
-    if (param_HEADLESS_PROCESS == "True"):
+    if (params['HEADLESS_PROCESS'] == "True"):
         referenceDIR = 'REF_HEADLESS'
     else:
         referenceDIR = 'REF_LIVE_MODE'
 
-    referenceImage = 'img/'+referenceDIR+'/'+listPWD[element]+'_REF.png'
+    referenceImage = 'img/'+referenceDIR+'/'+uniq_digits_in_pwd[element]+'_REF.png'
     referenceIMG = cv2.imread(referenceImage)
 
     # 2) Check for similarities between the 2 images
@@ -132,11 +134,11 @@ for element in range(len(listPWD)):
 
         if (s > 0.98):
             # print("SCORE=",score," LOCAL=",localImage,"REFERENCE=",referenceImage)
-            dictPWD[listPWD[element]]=localImage
+            dictPWD[uniq_digits_in_pwd[element]]=localImage
             break  #once we find the good match then skip to next pwd digit to search
 
 #-> click on each button corresponding to the password
-for digit in param_PWD:
+for digit in params['PWD']:
     element = dictPWD.get(digit)
     driver.find_element_by_id(element).click()
 
@@ -145,7 +147,7 @@ driver.find_element_by_id("valider").click()
 driver.get("https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_synthese/initPourCompteSelectionne-syntheseRelevesPDF.ea?compteNumero="+param_NumeroDeCompte)
 
 #-> before downloading the file, get the list of same file in the directory
-ListFileBefore = glob.glob(param_DownloadFolder+'\\releve_CCP*.pdf')
+ListFileBefore = glob.glob(params['DownloadFolder']+'\\releve_CCP*.pdf')
 
 driver.find_element_by_class_name(name="compte").click()
 driver.get("https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_synthese/refPDF-syntheseRelevesPDF.ea?indexCompteReleves=0&indexReleve=0")
@@ -154,7 +156,7 @@ time.sleep(10)  #let some seconds to download before stopping the webdriver
 driver.quit()   # kill the firefox instance
 
 #-> after downloading the file, get the list of same file in the directory
-ListFileAfter = glob.glob(param_DownloadFolder+'\\releve_CCP*.pdf')
+ListFileAfter = glob.glob(params['DownloadFolder']+'\\releve_CCP*.pdf')
 
 newFile = ( (list(set(ListFileAfter) - set(ListFileBefore))) )[0]
 
